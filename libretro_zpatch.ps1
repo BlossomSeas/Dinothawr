b057f691a8e1b0d30d402818a7346ca940166fc9
EvaMink <76566995+EvaMink@users.noreply.github.com>
2021-05-26 22:46:51 +0000

    Custom building

---
+++ libretro_zpatch.ps1
@@ -0,0 +1,14 @@
+git_merge timer_sync_option
+
+
+# ============================= #
+
+
+git_merge libretro_option
+
+
+git_resolve music_shuffle_option both retro_setenv_front.h retro_getvars_front.h retro_getvars.c
+git_resolve volume_slider_option both retro_setenv_front.h retro_getvars_front.h retro_getvars.c
+
+
+git_resolve audio_thread_option both retro_setenv_back.h retro_getvars_front.h retro_getvars.c
